ad03afb23f3a4e8dbd24ce1343efd866429d002f
PAPKE2019\Tilo <tilo.papke@abi06barth.de>
2024-02-18 23:46:19 +0100

    delete obsolete transformation code, updating mongodb dependency

--- src/main.rs
+++ src/main.rs
@@ -121,36 +121,6 @@ async fn main() {
 
 }
 
-#[allow(dead_code)]
-async fn http_server_simple() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-#[allow(dead_code)]
-async fn https_server_simple() {
-    let app = Router::new().route("/", get(|| async { "Hello, https world!" }));
-
-    // configure certificate and private key used by https
-    let config_async = RustlsConfig::from_pem_file(
-        "config/self-signed-certs/cert.pem",
-        "config/self-signed-certs/key.pem",
-    )
-    .await;
-
-    let config = config_async.unwrap();
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3333));
-
-    axum_server::bind_rustls(addr, config)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
-
 async fn http_server() {
     let local_setting: SettingStruct = SettingStruct::global().clone();
     let app = Router::new().route("/", get(http_handler));
